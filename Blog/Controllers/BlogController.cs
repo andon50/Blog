0ad11c3884a7661d383f5eab9ae33d3a0bdfcd4c
Anders Olofsson <andon50@gmail.com>
2020-10-23 12:17:51 +0200

    Fully functional!

--- Blog/Controllers/BlogController.cs
+++ Blog/Controllers/BlogController.cs
@@ -32,18 +32,20 @@ namespace Blog.Controllers
         }
 
         [HttpPost] //hämtar från vyn till controllern 
-        public IActionResult Create(Models.Blog blog)
+        public IActionResult Create(Models.Blog _blog)
         {
-            var _blog = new Models.Blog();
-            _blog.UserId = blog.UserId;
-            _blog.BlogPost = blog.BlogPost;
-            _blog.CategoryId = blog.CategoryId;
-            _blog.TimeStamp = DateTime.Now.ToString("yyyy/MM/dd H:mm");
+            var blog = new Models.Blog();
+            blog.UserId = _blog.UserId;
+            blog.BlogPost = _blog.BlogPost;
+            blog.CategoryId = _blog.CategoryId;
+            blog.TimeStamp = DateTime.Now.ToString("yyyy/MM/dd H:mm");
 
-            context.Blogs.Add(_blog);
+            context.Blogs.Add(blog);
             context.SaveChanges();
 
-            return View("Index");
+            //var blogs = context.Blogs.Include(u => u.User).Include(c => c.Category).OrderBy(t => t.TimeStamp).ToList();
+
+            return View("Msg", blog);
         }
 
     }
